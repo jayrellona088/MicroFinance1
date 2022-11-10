248b7f7cdfa0775cca5368ce483341aa61cd951c
Jayrell Ona <70945785+jayrellona088@users.noreply.github.com>
2022-11-10 10:14:32 +0800

    123

--- app/src/main/java/com/example/microfinance/Dashboard.java
+++ app/src/main/java/com/example/microfinance/Dashboard.java
@@ -9,13 +9,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
-    private LinearLayout l_Loan;
+    private LinearLayout l_Loan,l_EOQ,l_LoanHistory, l_AccountBook,l_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         l_Loan = findViewById(R.id.l_Loan);
+        l_EOQ = findViewById(R.id.l_EOQ);
+        l_LoanHistory = findViewById(R.id.l_LoanHistory);
+        l_AccountBook = findViewById(R.id.l_AccountBook);
+        l_profile = findViewById(R.id.l_profile);
+
+
 
         l_Loan.setOnClickListener(new View.OnClickListener() {
             @Override
